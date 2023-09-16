9e5550ad80f0239931793ac7ef51e21434daf005
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-09-16 19:14:09 +0200

    code: add function to render normal HTML into react
    Created a new react element to make it render inside the react as normal Html, without being a component

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementByIdr("oot")).render(<App />);
+const reactElement = {
+  type: "a",
+  props: {
+    href: "http://google.com",
+    target: "_blank",
+  },
+  children: "Click here to visit Google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom react App</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    Access Google
+  </a>
+);
+
+const aReactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click here to access Google"
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(aReactElement);
