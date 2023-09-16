989c7e0f3f2132bff8d9173e0271b81b92a19882
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-09-16 23:42:36 +0200

    feat(useState): add properly state management

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let counter = 15;
+  const [counter, setCounter] = useState(15);
 
   const addValue = () => {
-    counter += 1;
-    console.log(counter);
+    setCounter(counter + 1);
+  };
+
+  const removeValue = () => {
+    if (counter <= 0) return 0;
+    setCounter(counter - 1);
   };
 
   return (
@@ -13,7 +18,7 @@ function App() {
       <h1>React Project: Learning Hooks</h1>
       <h2>Counter value: {counter}</h2>
       <button onClick={addValue}>Add value</button>{" "}
-      <button>Remove value</button>
+      <button onClick={removeValue}>Remove value</button>
       <p>footer: {counter}</p>
     </>
   );
