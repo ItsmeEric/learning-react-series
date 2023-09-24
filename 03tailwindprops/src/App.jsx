6b960da34cbdb4e2676c58a9baef0de5705c94ad
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-09-24 13:46:32 +0200

    feat(Card): add new Card component and style it

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +10,7 @@ function App() {
       <h1 className="text-3xl bg-sky-700 p-3 rounded-md">
         Vite with TailwindCSS
       </h1>
+      <Card />
     </>
   );
 }
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+
+export default function Card() {
+  return (
+    <>
+      <img
+        src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="Glasses on the Desk with Open IDE with code, to  hack into a system"
+      />
+      <h1 className="text-3xl bg-sky-700 rounded">A card for photos</h1>
+      <p>
+        Be good, hack with ethics, don't cross the boundaries of what is wrong
+        or right.
+      </p>
+    </>
+  );
+}
