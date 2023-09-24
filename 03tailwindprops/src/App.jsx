d8b92092f8ee6857fe62b471411a3c5bdc17b000
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-09-24 14:23:09 +0200

    code: add props to card component
    Added the username and role as props to card component, and added a default value

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-3xl bg-sky-700 p-3 rounded-md">
         Vite with TailwindCSS
       </h1>
+      <Card username="Eric Mbarushimana" role="Fraud Analyst, Mourway" />
       <Card />
     </>
   );
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,17 +1,31 @@
 /* eslint-disable react/no-unescaped-entities */
+import React from "react";
 
-export default function Card() {
+export default function Card({ username = "EM", role = "Not assigned yet" }) {
   return (
     <>
-      <img
-        src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        alt="Glasses on the Desk with Open IDE with code, to  hack into a system"
-      />
-      <h1 className="text-3xl bg-sky-700 rounded">A card for photos</h1>
-      <p>
-        Be good, hack with ethics, don't cross the boundaries of what is wrong
-        or right.
-      </p>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img
+          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+          src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. It’s easy to customize, adapts to any design, and the build
+              size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">{username}</div>
+            <div className="text-slate-700 dark:text-slate-500">{role}</div>
+          </figcaption>
+        </div>
+      </figure>
     </>
   );
 }
