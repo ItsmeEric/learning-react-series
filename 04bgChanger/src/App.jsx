f6e91884e381334aee618f3567f325359e74c6a8
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-09-26 22:54:46 +0200

    feat: add state management to the button

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,12 +1,33 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("red");
+
+  // One of the methods to change color
+  // function changeColor(color) {
+  //   setColor(color);
+  // }
+
   return (
-    <div className="w-full h-screen duration-200">
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button>Test</button>
+          <button
+            className="outline-none px-4 py-1 rounded-s-full shadow-lg text-black"
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-s-full shadow-lg text-black"
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
         </div>
       </div>
     </div>
