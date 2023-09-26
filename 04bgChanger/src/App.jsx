789c784b3b4c9783d09fbe74055276c0b396ce86
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-09-26 23:06:33 +0200

    style(button): style buttons backgroundColor

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [color, setColor] = useState("red");
@@ -17,16 +16,25 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-            className="outline-none px-4 py-1 rounded-s-full shadow-lg text-black"
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
             onClick={() => setColor("red")}
+            style={{ backgroundColor: "red" }}
           >
             Red
           </button>
           <button
-            className="outline-none px-4 py-1 rounded-s-full shadow-lg text-black"
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-black"
             onClick={() => setColor("blue")}
+            style={{ backgroundColor: "blue" }}
           >
             Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+            onClick={() => setColor("black")}
+            style={{ backgroundColor: "black" }}
+          >
+            Black
           </button>
         </div>
       </div>
