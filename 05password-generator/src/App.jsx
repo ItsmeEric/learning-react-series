28978caf55262ea811a15e65bf2fb2042d363fff
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-18 00:14:06 +0200

    🎉[Feauture]: add random number generator to password

--- 05password-generator/src/App.jsx
+++ 05password-generator/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+";
+
+    for (let i = 0; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
 
   return (
     <div
