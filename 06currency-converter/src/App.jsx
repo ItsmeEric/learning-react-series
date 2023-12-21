eab53aee7e5d9a8e4391ed20e7a0e99d8a3b4940
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-21 23:52:16 +0200

    🎉[Feature]: add background image

--- 06currency-converter/src/App.jsx
+++ 06currency-converter/src/App.jsx
@@ -7,9 +7,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div
+      className="w-full h-screen flex flex-wrap 
+    justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://images.pexels.com/photos/259251/pexels-photo-259251.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`,
+      }}
+    >
       <h1>Currency Converter</h1>
-    </>
+    </div>
   );
 }
 
