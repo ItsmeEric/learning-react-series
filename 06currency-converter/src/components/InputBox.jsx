bcecfd8b95c77232b5562f15f8babb306eba6240
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-22 00:12:47 +0200

    🎉[Feature]: fill the input form and label with necessary states

--- 06currency-converter/src/components/InputBox.jsx
+++ 06currency-converter/src/components/InputBox.jsx
@@ -8,8 +8,24 @@ export default function InputBox(
   currencyOptions = [],
   selectedCurrency = "usd",
   amountDisabled = false,
-  currecnyDisabled = false,
+  currencyDisabled = false,
   className = ""
 ) {
-  return <div></div>;
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1-2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          type="number"
+          className="outline-none w-full bg-transparent py-1.5"
+          placeholder="Amount"
+          disabled={amountDisabled}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+    </div>
+  );
 }
