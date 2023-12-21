86144fd19b84c17a571b8ab3e408f456430db3c1
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-22 00:00:01 +0200

    🎉[Feature]: create input box component

---
+++ 06currency-converter/src/components/InputBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function InputBox(
+  label,
+  amount,
+  onAmountChange,
+  OnCurrencyChange,
+  currencyOptions = [],
+  selectedCurrency = "usd",
+  amountDisabled = false,
+  currecnyDisabled = false,
+  className = ""
+) {
+  return <div></div>;
+}
