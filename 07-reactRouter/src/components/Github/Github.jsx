abf181b5eb58b7adb34ada44292552c0fd344d6a
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-01 23:57:03 +0200

    🎉[Feature]: create Github component

---
+++ 07-reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Github() {
+  return (
+    <div className="bg-orange-500 text-black text-3xl text-center py-5">
+      Github
+    </div>
+  );
+}
+
+export default Github;
