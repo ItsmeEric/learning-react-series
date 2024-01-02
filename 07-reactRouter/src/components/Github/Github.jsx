419a66548f2cdb26865a1c1e351f0e00696d226c
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-02 22:01:31 +0200

    🎉[Feature]: fetch github users info using async await

--- 07-reactRouter/src/components/Github/Github.jsx
+++ 07-reactRouter/src/components/Github/Github.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = React.useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/itsmeeric")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData();
+  // const [data, setData] = React.useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/itsmeeric")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     });
+  // }, []);
 
   return (
     <div className="bg-orange-500 text-black text-3xl text-center py-5">
@@ -20,3 +22,8 @@ function Github() {
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/itsmeeric");
+  return response.json();
+};
