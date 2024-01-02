b166430705ff9c8709748f57d9a10f8abd517c87
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-02 21:54:41 +0200

    🎉[Feature]: fetch github users info

--- 07-reactRouter/src/components/Github/Github.jsx
+++ 07-reactRouter/src/components/Github/Github.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Github() {
+  const [data, setData] = React.useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/itsmeeric")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
   return (
     <div className="bg-orange-500 text-black text-3xl text-center py-5">
-      Github
+      Github followers count: {data.followers}
+      <img src={data.avatar_url} width={250} alt="github profile image" />
     </div>
   );
 }
