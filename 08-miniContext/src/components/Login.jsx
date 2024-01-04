335e9af96241cbed9c47dec8e224d27664f81234
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-04 23:53:59 +0200

    🎉[Feature]: add useContext to Login component
    Added useContext to handle context api file component that comes from context provider

--- 08-miniContext/src/components/Login.jsx
+++ 08-miniContext/src/components/Login.jsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const { setUser } = useContext(UserContext);
+
   const handleSubmit = (e) => {
-    // Prevent the page from refreshing
+    e.preventDefault();
+    setUser({ username, password });
   };
+
   return (
     <div>
       <h2>Login</h2>
       <input
         type="text"
+        value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Username"
       />{" "}
