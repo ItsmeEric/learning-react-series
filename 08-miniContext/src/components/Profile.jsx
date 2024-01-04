8247d12907a16e90722e58c517e8661a88a3cbc9
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-04 23:57:53 +0200

    🎉[Feature]: add user coming from context api provider

--- 08-miniContext/src/components/Profile.jsx
+++ 08-miniContext/src/components/Profile.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <h1>Not logged in</h1>;
+
   return (
     <div>
-      Profile: {}
+      Profile: {user}
       <h1>More components</h1>
     </div>
   );
