82af0acf6141e99f6d6590e0ca9ca18dea2226c2
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-09 00:24:31 +0200

    📖[Docs]: wrap the app with context provider and set its'initial state

--- 10-todoLocalStorage/src/App.jsx
+++ 10-todoLocalStorage/src/App.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { useState } from "react";
+import { TodoProvider } from "./contexts";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleTodo }}
+    >
       <h1 className="text-3xl font-bold underline">Hello Eric!</h1>
-    </>
+    </TodoProvider>
   );
 }
 
