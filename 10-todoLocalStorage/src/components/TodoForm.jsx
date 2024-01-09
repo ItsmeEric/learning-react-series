d2565101115a355d5503976879b40b4f1647613c
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-10 00:25:09 +0200

    📖[Docs]: add todo form logic

---
+++ 10-todoLocalStorage/src/components/TodoForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+
+function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+
+  const add = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
+  return <div>TodoForm</div>;
+}
+
+export default TodoForm;
