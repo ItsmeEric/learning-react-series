b5aceea20a7abf58a4e5bdaa346fd58d3b40a531
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-12 23:54:18 +0200

    🎉[Feature]: add remove todo function

--- 11-todoapp-with-redux/src/components/Todos.js
+++ 11-todoapp-with-redux/src/components/Todos.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 
 function Todos() {
   const todos = useSelector((state) => state.todos);
-  console.log(todos);
+
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -15,7 +18,10 @@ function Todos() {
             key={todo.id}
           >
             <div className="text-white">{todo.text}</div>
-            <button className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
