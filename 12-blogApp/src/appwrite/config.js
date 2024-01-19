554638c988a6f1bfe99be95777d5ba0d4f2c738b
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-19 14:25:22 +0200

    🎉[Feature]: create function to update the posts

--- 12-blogApp/src/appwrite/config.js
+++ 12-blogApp/src/appwrite/config.js
@@ -57,6 +57,25 @@ export class Service {
       return false;
     }
   }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: updateDocument() :: ", error);
+      return false;
+    }
+  }
 }
 
 // Init SDK
