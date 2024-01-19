96fa049727639042160af725d934577aeefbe748
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-19 14:29:19 +0200

    📖[Docs]: delete unnecasary line of codes

--- 12-blogApp/src/appwrite/config.js
+++ 12-blogApp/src/appwrite/config.js
@@ -91,13 +91,3 @@ export class Service {
     }
   }
 }
-
-// Init SDK
-// const client = new Client();
-
-// const databases = new Databases(client);
-
-// client
-//   .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
-//   .setProject("5df5acd0d48c2") // Your project ID
-//   .setKey("919c2d18fb5d4...a2ae413da83346ad2"); // Your secret API key
