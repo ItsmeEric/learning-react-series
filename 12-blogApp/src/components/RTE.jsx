7e9fabc66a58bfbb67a5f5a0d84467c266597f82
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-27 23:59:43 +0200

    🎉[Feature]: create RTE component and add its functionality

---
+++ 12-blogApp/src/components/RTE.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
+
+function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onchange } }) => {
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              branding: false,
+              height: 500,
+              menubar: true,
+            }}
+          />;
+        }}
+      />
+    </div>
+  );
+}
+
+export default RTE;
