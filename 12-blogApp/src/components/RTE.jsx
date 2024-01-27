b66c8659e08467fafe217ca0aadecf17f2710172
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-28 00:18:13 +0200

    🎉[Feature]: add finishing touches to Real Time Editor (RTE) component

--- 12-blogApp/src/components/RTE.jsx
+++ 12-blogApp/src/components/RTE.jsx
@@ -16,7 +16,35 @@ function RTE({ name, control, label, defaultValue = "" }) {
               branding: false,
               height: 500,
               menubar: true,
+              plugins: [
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | " +
+                "bold italic forecolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             }}
+            onEditorChange={onchange}
           />;
         }}
       />
