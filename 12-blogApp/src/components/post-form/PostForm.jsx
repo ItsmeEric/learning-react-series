759fc1f7587b3c03b167333527a4a49530a3bfd1
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-30 23:05:38 +0200

    🎉[Feature]: add aditional post form information

--- 12-blogApp/src/components/post-form/PostForm.jsx
+++ 12-blogApp/src/components/post-form/PostForm.jsx
@@ -22,7 +22,37 @@ export default function PostForm({ post }) {
   const navigate = useNavigate();
   const userData = useSelector((state) => state.auh.userData);
 
-  const submit = async (data) => {};
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? await appwriteService.uploadFile(data.image[0])
+        : null;
+
+      if (file) {
+        appwriteService.deleteFile(post.featuredImage);
+      }
+      const dbPost = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+      if (dbPost) {
+        navigate(`/post/${dbPost.$id}`);
+      }
+    } else {
+      const file = await appwriteService.uploadFile(data.imagep[0]);
+      if (file) {
+        const fileId = file.$id;
+        data.featuredImage = fileId;
+        const dbPost = await appwriteService.createPost({
+          ...data,
+          userId: userData.$id,
+        });
+        if (dbPost) {
+          navigate(`/post/${dbPost.$id}`);
+        }
+      }
+    }
+  };
 
   const slugTransform = useCallback((value) => {
     if (value && typeof value === "string")
