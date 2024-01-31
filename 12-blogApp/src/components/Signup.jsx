df23cb326a21ff4ea30e6b7071af15690027a84d
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-01-31 22:53:23 +0200

    🎉[Feature]: create signup component and import necessary files

---
+++ 12-blogApp/src/components/Signup.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import authService from "../appwrite/auth";
+import Button from "./Button";
+import Input from "./Input";
+import Logo from "./Logo";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+
+function Signup() {
+  return <div>Signup</div>;
+}
+
+export default Signup;
