658f55a77ae2cce6a8b7b77bed4cedeac9766541
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-02-01 23:49:23 +0200

    🎉[Feature]: add functionality to let user create account and login

--- 12-blogApp/src/components/Signup.jsx
+++ 12-blogApp/src/components/Signup.jsx
@@ -6,8 +6,26 @@ import Input from "./Input";
 import Logo from "./Logo";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
+import { login } from ".../store/authSlice";
 
 function Signup() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const create = async (data) => {
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if (userData) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(login({ userData }));
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   return <div>Signup</div>;
 }
 
