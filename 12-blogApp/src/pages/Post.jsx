0b1cc786402d76d60c9fcdda45b8571db289b292
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-02-05 23:19:11 +0200

    📖[Docs]: import necessary files

---
+++ 12-blogApp/src/pages/Post.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config";
+import Button from "../components/Button";
+import Container from "../components/container/Container";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
+
+function Post() {
+  return <div>Post</div>;
+}
+
+export default Post;
