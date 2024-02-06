ad67717b42795632688a4041a720b0c781371edc
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2024-02-06 11:38:05 +0200

    📖[Docs]: create initial variabled and methods for deleting post and checking the author

--- 12-blogApp/src/pages/Post.jsx
+++ 12-blogApp/src/pages/Post.jsx
@@ -7,6 +7,21 @@ import parse from "html-react-parser";
 import { useSelector } from "react-redux";
 
 function Post() {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  const deletePost = () => {
+    appwriteService.deletePost(post.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(post.featuredImage);
+        navigate("/");
+      }
+    });
+  };
+
   return <div>Post</div>;
 }
 
